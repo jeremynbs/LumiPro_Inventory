fe27a114cb4a2088753d7c303c8f5ed2d58f3720
Jeremy <jeremy.budiono@gmail.com>
2025-12-19 12:42:07 +0800

    render

--- app.py
+++ app.py
@@ -999,5 +999,7 @@ def inventory():
 
 # -------------------------------- RUN THE APP --------------------------------
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    # Get port from environment variable or default to 5000
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
