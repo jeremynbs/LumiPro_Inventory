d99b6bfa5a1b64907a81374a4ceda02cc8351ecd
Jeremy <jeremy.budiono@gmail.com>
2025-12-19 18:11:12 +0800

    3.12

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -5,8 +5,7 @@ import os
 sys.path.insert(0, os.path.dirname(__file__))
 
 # 2. Point to the virtual environment libraries
-# Replace 'jerepnkw' with your username and '3.10' with your version
-sys.path.insert(0, '/home/jerepnkw/virtualenv/repositories/LumiPro_Inventory/3.13/lib/python3.13/site-packages')
+sys.path.insert(0, '/home/jerepnkw/virtualenv/lumipro.jeremynbs.org/3.12/lib/python3.12/site-packages')
 
 # 3. Import your app
 from app import app as application
