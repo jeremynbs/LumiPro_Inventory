d8b82e99987502268c5d60fb9d993dc3d7325416
Jeremy <jeremy.budiono@gmail.com>
2025-12-19 15:18:38 +0800

    update flask run

--- app.py
+++ app.py
@@ -1013,7 +1013,6 @@ def inventory():
 
 # -------------------------------- RUN THE APP --------------------------------
 
-if __name__ == "__main__":
-    # Get port from environment variable or default to 5000
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app = Flask(__name__)
+    app.run(debug=True)
